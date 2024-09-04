1666d272dd6f57fbae5ccf8bef6a5e101c9feedb
Khalid-Elejla <khalidelejla@gmail.com>
2024-09-04 12:46:07 +0300

    add streamlit camera

--- control_center/alerts.py
+++ control_center/alerts.py
@@ -7,3 +7,8 @@ def alerts_page():
         st.write(f"Welcome, {st.session_state['selected_name']}!")
     else:
         st.write("No name selected yet.")
+
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)
